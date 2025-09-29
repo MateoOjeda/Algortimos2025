303ce7058a988c7409017c791b4e7bd2c00c71f7
MateoOjeda <82356736+MateoOjeda@users.noreply.github.com>
2025-09-29 11:54:51 -0300

    Delete Practica(Lista) directory

--- Practica(Lista)/practica_lista13.py
+++
@@ -1,3 +0,0 @@
-# 13.Desarrollar un algoritmo que permita visualizar el contenido de una lista de manera ascendente
-#    y descendente de sus elementos, debe modificar el TDA para implementar lista doblemente
-#    enlazada.
